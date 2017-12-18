789d5730aa898bc46bd64030d10e586426be780c
Rodion <madnessjust4@gmail.com>
2017-12-18 18:35:07 +0300

    Send mail test

---
+++ Platform_for_testing_code_quality/src/main/java/com/MainLogic/Main.java
@@ -0,0 +1,15 @@
+package com.MainLogic;
+
+
+public class Main
+{
+    private static SenderTls tlsSender = new SenderTls("justmadnessyo@gmail.com", "");
+    private static SenderSsl sslSender = new SenderSsl("justmadnessyo@gmail.com", "");
+
+    public static void main(String[] args)
+    {
+        tlsSender.send("This is Subject", "TLS: This is text!", "madnessjustyo@gmail.com", "madnessjust4@gmail.com");
+        sslSender.send("This is Subject", "SSL: This is text!", "madnessjustyo@gmail.com", "madnessjust4@gmail.com");
+        System.out.print("Done");
+    }
+}
